066c7f6b1a9f0e967ed3bc6b8dd7d1c558e7e892
Local Al <alexander.s.woolley@gmail.com>
2021-01-25 19:58:33 -0700

    tests build

---
+++ tests/Manager.test.js
@@ -0,0 +1,22 @@
+function ManagerTest(name, id, email, office) {
+    if (typeof name !== "string" || !name.trim().length) {
+      throw new Error("Expected parameter 'name' to be a non-empty string");
+    }
+  
+    if (typeof id !== "number" || isNaN(id) || id < 0) {
+      throw new Error("Expected parameter 'id' to be a non-negative number");
+    }
+
+    if (typeof email !== "string" || !email.trim().length) {
+        throw new Error("Expected parameter 'email' to be a non-empty string");
+      }
+
+      if (typeof office !== "number" || isNaN(offcie) || office < 0) {
+        throw new Error("Expected parameter 'office' to be a non-negative number");
+      }
+  
+    this.name = name;
+    this.id = id;
+  }
+  
+  module.exports = ManagerTest;
