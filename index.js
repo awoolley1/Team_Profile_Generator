b0b36029de1de1d2f6726072a53b6d4e284a24bf
Local Al <alexander.s.woolley@gmail.com>
2021-01-25 18:21:10 -0700

    html skeleton added

--- index.js
+++ index.js
@@ -2,34 +2,80 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const jest = require("jest")
-const generateHTML = require("./utils/generateHTML")
-const Employee = require("../library/Employee")
-const Manager = require("../library/Manager")
-const Engineer = require("../library/Manager")
-const Intern = require("../library/Intern")
+const generateHTML = require("./generateHTML")
+const Employee = require("./library/Employee")
+const Manager = require("./library/Manager")
+const Engineer = require("./library/Manager")
+const Intern = require("./library/Intern")
 
 
 
 const managerQuestions = [
   {
     type: "input",
-    name: "name",
+    name: "managername",
     message: "Enter manager's name:",
   },
   {
     type: "input",
-    name: "id",
+    name: "managerid",
     message: "Enter manager's employee ID:",
   },
   {
     type: "input",
-    name: "email",
+    name: "manageremail",
     message: "Enter manager's email address:",
   },
   {
     type: "input",
-    name: "office",
+    name: "manageroffice",
     message: "Enter manager's office number:",
+  },
+]
+
+const engineerQuestions = [
+  {
+    type: "input",
+    name: "engineername",
+    message: "Enter engineer's name:",
+  },
+  {
+    type: "input",
+    name: "engineerid",
+    message: "Enter engineer's employee ID:",
+  },
+  {
+    type: "input",
+    name: "engineeremail",
+    message: "Enter engineer's email address:",
+  },
+  {
+    type: "input",
+    name: "engineergithub",
+    message: "Enter engineer's GitHub Username:",
+  },
+]
+
+const internQuestions = [
+  {
+    type: "input",
+    name: "internname",
+    message: "Enter interns's name:",
+  },
+  {
+    type: "input",
+    name: "internid",
+    message: "Enter intern's employee ID:",
+  },
+  {
+    type: "input",
+    name: "internemail",
+    message: "Enter intern's email address:",
+  },
+  {
+    type: "input",
+    name: "internschool",
+    message: "Enter intern's school:",
   },
 ]
 
@@ -37,26 +83,26 @@ const managerQuestions = [
 function init() {
   inquirer
     .prompt([
-      {
-        type: "input",
-        name: "ManagerName",
-        message: "Enter manager's name:",
-      },
-      {
-        type: "input",
-        name: "ManagerID",
-        message: "Enter manager's employee ID:",
-      },
-      {
-        type: "input",
-        name: "ManagerEmail",
-        message: "Enter manager's email address:",
-      },
-      {
-        type: "input",
-        name: "ManagerOffice",
-        message: "Enter manager's office number:",
-      },
+      // {
+      //   type: "input",
+      //   name: "ManagerName",
+      //   message: "Enter manager's name:",
+      // },
+      // {
+      //   type: "input",
+      //   name: "ManagerID",
+      //   message: "Enter manager's employee ID:",
+      // },
+      // {
+      //   type: "input",
+      //   name: "ManagerEmail",
+      //   message: "Enter manager's email address:",
+      // },
+      // {
+      //   type: "input",
+      //   name: "ManagerOffice",
+      //   message: "Enter manager's office number:",
+      // },
   
     ])
     .then(function (responses) {
