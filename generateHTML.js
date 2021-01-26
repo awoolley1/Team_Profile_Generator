3edeab846c4fc99cdba1842b1ce460729ab2df17
Local Al <alexander.s.woolley@gmail.com>
2021-01-25 20:26:19 -0700

    generateHTML forloop + conditional

--- generateHTML.js
+++ generateHTML.js
@@ -1,42 +1,41 @@
 
-
-// Create a function to generate HTML file
 function generateHTML(responses) {
 
+//write for loop to create right quantity of cards 
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+  }
+
+//write conditional based on which role is being input
+
   return `
-// # **Title: ${responses.Title}**
-
-// ${licenseBadgeURL}
-// #### **Table of Contents:**
-// 1. [*Description*](#description: "Jump to Description")
-// 2. [*Installation*](#installation: "Jump to Installation")
-// 3. [*Usage*](#usage: "Jump to Usage")
-// 4. [*Contributing*](#contributing: "Jump to Contributing")
-// 5. [*Tests*](#tests: "Jump to Tests")
-// 6. [*Licenses*](#license: "Jump to Licenses")
-// 7. [*Questions*](#questions: "Jump to Questions")
+  // <!DOCTYPE html>
+  // <html lang="en">
+  // <head>
+  //     <meta charset="UTF-8">
+  //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  //     <title>Document</title>
+  //     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  // </head>
+  // <body>
+  //     <div class="card" style="width: 18rem;">
+  //         <div class="card-body">
+  //           <h5 class="card-title">Name:</h5>
+  //           <p class="card-text">Role: </p>
+  //         </div>
+  //         <ul class="list-group list-group-flush">
+  //           <li class="list-group-item">ID:</li>
+  //           <li class="list-group-item">Email:</li>
+  //           <li class="list-group-item">Office, School, GitHub:</li>
+  //         </ul>
+  //       </div>
   
-// ### **Description:**
-// ${responses.Description}
-  
-// ### **Installation:**
-// ${responses.Installation}
-  
-// ### **Usage:**
-// ${responses.Usage}
-  
-// ### **Contributing:**
-// ${responses.Contributing}
-  
-// ### **Tests:**
-// ${responses.Tests}
-  
-// ### **License:**
-// ${responses.License}
-  
-// ### **Questions:**
-// Contact me on GitHub @ ${responses.GitHub} or by Email at ${responses.Email}
-
+  //       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  //       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  //       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  //     </body>
+  </html>
 `;
 }
 
