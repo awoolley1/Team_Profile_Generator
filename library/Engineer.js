dcf292f24591e6eb51bfce521ac0e51c453910f7
Local Al <alexander.s.woolley@gmail.com>
2021-01-27 14:13:44 -0700

    simple formatting

--- library/Engineer.js
+++ library/Engineer.js
@@ -1,17 +1,16 @@
 const Employee = require("./Employee");
 
 class Engineer extends Employee {
-    constructor(name, id, email, gitHub) {
-        super(name, id, email);
-        this.gitHub=gitHub;
-    }
-    getRole() {
-        return "Engineer"
-    }
-    getGithub() {
-        return this.gitHub
-    }
+  constructor(name, id, email, gitHub) {
+    super(name, id, email);
+    this.gitHub = gitHub;
+  }
+  getRole() {
+    return "Engineer";
+  }
+  getGithub() {
+    return this.gitHub;
+  }
 }
 
-
-module.exports =  Engineer;
+module.exports = Engineer;
