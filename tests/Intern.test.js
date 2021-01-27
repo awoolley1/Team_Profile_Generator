f253366e160a662b1934c2264f60b58fd2b114eb
Local Al <alexander.s.woolley@gmail.com>
2021-01-27 11:35:23 -0700

    file cleanup

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -8,11 +8,11 @@ function InternTest(name, id, email, school) {
     }
 
     if (typeof email !== "string" || !email.trim().length) {
-        throw new Error("Expected parameter 'name' to be a non-empty string");
+        throw new Error("Expected parameter 'email' to be a non-empty string");
     }
 
     if (typeof school !== "string" || !school.trim().length) {
-        throw new Error("Expected parameter 'name' to be a non-empty string");
+        throw new Error("Expected parameter 'school' to be a non-empty string");
     }
   
     this.name = name;
